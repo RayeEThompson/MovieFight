aecf4bd33fd19f07d8ddc45c2ecc09676085625a
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2020-06-27 13:32:20 -0500

    update for axios request

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "9b951bc5",
+      s: "avengers",
+    },
+  });
+  console.log(response.data);
+};
+fetchData();
